6428d511c8aa34fcd0e7c881f548aec78110b65e
Xi Yang <69822082+GrignardReagent@users.noreply.github.com>
2023-10-23 14:19:43 +0100

    Delete fastqc_out directory

--- fastqc_out/STEP0_copy_fastq.sh
+++
@@ -1,4 +0,0 @@
-# This script copies the fastq files necessary for Step 1
-cp  /localdisk/data/BPSM/ICA1/fastq . 
-cd fastq/ 
-echo -e "STEP 0 complete, please execute STEP1_run_fastqc.sh next"
--- fastqc_out/unzip_all.sh
+++
@@ -1,3 +0,0 @@
-for file in *.zip; do
-	unzip ${file}
-done
